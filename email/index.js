a84a0e57578998fb621c821a14c082786a794e1f
kaci-ldww <117759041+kaci-ldww@users.noreply.github.com>
2026-09-14 12:29:32 -0600

    Update index.js

--- email/index.js
+++ email/index.js
@@ -22,16 +22,16 @@ const CLIENT_CONFIG = {
                 desc: "artist_dropdown", type2:[ "touch2","touch3","touch4","touch5","touch6a","touch6b","touch7" ]},
             { label: "Gaming",          code: "gaming",         utmCampaign: "gaming",
                 content: ["promotion","mywinstar","clubpassport","influencer","winstarwinners"],
-                desc: ["membership","awareness","ladyluck","nuture"], type2: ["newmember","uncarded","carded","prospect","touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
+                desc: ["membership","awareness","ladyluck","nurture"], type2: ["newmember","uncarded","carded","prospect","touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
             { label: "Brand Awareness", code: "brandawareness", utmCampaign: "brandawareness",
                 content: ["reengagement","gozone","welcome","giveaway","winstarwinners"],
-                desc: ["reengagement1","reengagement2","reengagement3","nuture"], type2: ["touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
+                desc: ["reengagement1","reengagement2","reengagement3","nurture"], type2: ["touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
             { label: "Amenities",       code: "amenities",      utmCampaign: "hotel",
                 content: ["hotel","spa","pool","dining","meet","winstarwinners"],
-                desc: ["retailpackage","conventions","weddings","catering","prestay1","prestay2","nuture"], type2: ["couples","touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
+                desc: ["retailpackage","conventions","weddings","catering","prestay1","prestay2","nurture"], type2: ["couples","touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] },
             { label: "Golf",            code: "golf",           utmCampaign: "golf",
                 content: ["instruction","retailpackage","membership","tournaments","winstarwinners"],
-                desc: ["email1","email2","email3","nuture"], type2: ["touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] }
+                desc: ["email1","email2","email3","nurture"], type2: ["touch2","touch3","touch4","touch5","touch6a","touch6b","touch7"] }
         ]
     },
     rw: {
